fdb80531abc7d1afd88ed29ad9b5489a659c4ce2
Cody Bouscaren & alex caste <student@epicodus.com>
2015-04-27 12:23:39 -0700

    code with attempts to merge.  without merge hash works for 1 key 1 value

--- lib/my_hash.rb
+++ lib/my_hash.rb
@@ -1,12 +1,32 @@
 class MyHash
-    define_method(:initialize) |key, value| do
+    define_method(:initialize) do
+        @store_array = []
+    end
+
+    define_method(:store) do |key, value|
         @key = key
         @value = value
+        @store_array.push(key)
+        @store_array.push(value)
+        @store_array
     end
 
     define_method(:fetch) do |key|
-        @key
-
-
+        @store_array[1]
     end
+
+    define_method(:has_key?) do |key|
+        if @store_array[0] == key
+            true
+        end
+    end
+
+    define_method(:has_value?) do |value|
+        if @store_array[1] == value
+            true
+        end
+    end
+
+    #define_method(:merge) do |array|
+
 end
--- spec/my_hash_spec.rb
+++ spec/my_hash_spec.rb
@@ -10,4 +10,38 @@ describe(MyHash) do
             expect(test_hash.fetch("kitten")).to(eq("cute"))
         end
     end
+
+    describe("#has_key?") do
+        it("returns true if the given input matches the key") do
+            test_hash = MyHash.new()
+            test_hash.store("kitten", "cute")
+            expect(test_hash.has_key?("kitten")).to(eq(true))
+        end
+    end
+
+    describe("#has_value?") do
+        it("returns true if the given input matches the value") do
+            test_hash = MyHash.new()
+            test_hash.store("kitten", "cute")
+            expect(test_hash.has_value?("cute")).to(eq(true))
+        end
+    end
+
+    # describe("#merge") do
+    #     it("adds the input key and value to the end of the initial array") do
+    #         test_hash = MyHash.new()
+    #         test_hash.store("kitten", "cute")
+    #         test_hash_two = MyHash.new()
+    #         test_hash_two.store("dog", "large")
+    #         expect(test_hash.merge(test_hash_two)).to(eq(["kitten", "cute", "dog", "large"]))
+    #     end
+    # end
+    #
+    # describe("#merge") do
+    #     it("adds the replaces the value within array if the input key is the same") do
+    #         test_hash = MyHash.new()
+    #         test_hash.store("kitten", "cute")
+    #         expect(test_hash.merge("kitten", "large")).to(eq(["kitten", "large"]))
+    #     end
+    end
 end
